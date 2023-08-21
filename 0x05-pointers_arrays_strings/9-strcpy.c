f78165f4f0216ee7494a6a009a266fe43cf66819
Mohamed Noor <mhdnrr@gmail.com>
2023-08-21 12:32:31 -0700

    All

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * The string point
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int l = 0;
+
+        int x = 0;
+
+        while (*(src + l) != '\0')
+        {
+                l++;
+
+        
+
+        for ( ; x < l ; x++)
+
+        {
+
+                dest[x] = src[x];
+
+        }
+
+        dest[l] = '\0';
+
+        return (dest);
+
+}
