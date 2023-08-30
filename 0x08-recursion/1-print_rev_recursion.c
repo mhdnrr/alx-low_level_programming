150e48446d0a771b77e8ababd403ff2988402c67
Mohamed Noor <mhdnrr@gmail.com>
2023-08-30 05:36:10 -0700

    All

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
