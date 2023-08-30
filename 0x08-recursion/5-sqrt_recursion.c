dd56384eab0e4626a228e190df43a5412930330a
Mohamed Noor <mhdnrr@gmail.com>
2023-08-30 06:14:19 -0700

    All

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,6 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root
- * @i: iterator
  * Return: the resulting square root
  */
  
@@ -13,6 +12,13 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural square root of a number
+ * @n: number to calculate the sqaure root
+ * @i: iterator
+ * Return: the resulting square root
+ */
 
 int actual_sqrt_recursion(int n, int i)
 {
