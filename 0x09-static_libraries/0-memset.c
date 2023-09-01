e8b92d881a3d10986abd014ced71599e086af624
Mohamed Noor <mhdnrr@gmail.com>
2023-09-01 14:52:47 -0700

    All

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block 
+ * Return: changed array with new value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
