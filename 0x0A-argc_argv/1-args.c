574d7e584eba6cedc801d528b12df63cf78b2aaf
mhdnrr <mhdnrr@gmail.com>
2023-09-23 07:27:02 -0700

    All

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,9 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
+ * main - print the number
+ * @argv: argv
+ * @argc: argc
  * Return: Always 0 (Success)
  */
 
