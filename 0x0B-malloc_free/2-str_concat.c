7a8353ee0e3de88451b5164d845f2d25b017b8e7
mhdnrr <mhdnrr@gmail.com>
2023-09-24 03:26:56 -0700

    updating

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,41 +2,43 @@
 #include <stdlib.h>
 
 /**
- * str_concat - get ends of input and add together
+ * str_concat - get ends of input
+ * @s1: s1
+ * @s2: s2
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *conct;
-    int i, ci;
+	char *conct;
+	int i, ci;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
 
-    i = ci = 0;
-    while (s1[i] != '\0')
-        i++;
-    while (s2[ci] != '\0')
-        ci++;
-    conct = malloc(sizeof(char) * (i + ci + 1));
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-    if (conct == NULL)
-        return (NULL);
-    i = ci = 0;
-    while (s1[i] != '\0')
-    {
-        conct[i] = s1[i];
-        i++;
-    }
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
 
-    while (s2[ci] != '\0')
-    {
-        conct[i] = s2[ci];
-        i++, ci++;
-    }
-    conct[i] = '\0';
-    return (conct);
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
 }
 
